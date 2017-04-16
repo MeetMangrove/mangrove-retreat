6799f570e892a6dec861de69de8d8f323d15e967
AmbroiseCollon <ambroise.collon@gmail.com>
2017-04-16 10:39:35 +0200

    Added redirect URI in slack oauth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,13 @@
 const request = require('request')
 const clientId = '24629294631.166278537604'
 const clientSecret = '7fedc702b17499b262d1bab10d1fa7ad'
+const slackRedirectUri = process.env.SLACK_REDIRECT_URI ? process.env.SLACK_REDIRECT_URI : 'http://localhost:3000/auth'
 const airtable = require('../helpers/airtable.js')
 
 function checkCode(code) {
 	return new Promise(function (resolve, reject) {
-		request.get('https://slack.com/api/oauth.access?code=' + code + '&client_id=' + clientId + '&client_secret=' + clientSecret,
+		request.get('https://slack.com/api/oauth.access?code=' + code + '&client_id=' + clientId
+		+ '&client_secret=' + clientSecret + '&redirect_uri=' + slackRedirectUri,
 			function (error, response, body) {
 				if (!error && response.statusCode == 200) {
 					const b = JSON.parse(body)
