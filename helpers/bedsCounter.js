6ad4ffbf9ced6ca4f4783604f52d2d734c1def31
AmbroiseCollon <ambroise.collon@gmail.com>
2017-05-10 16:19:03 +0200

    Fixed beds count crash

--- helpers/bedsCounter.js
+++ helpers/bedsCounter.js
@@ -1,8 +1,8 @@
 function haveDaysInCommon(days1, days2) {
 	for (var i = 0; i < days1.length; i++) {
 		var day1 = days1[i]
-		for (var i = 0; i < days2.length; i++) {
-			var day2 = days2[i]
+		for (var j = 0; j < days2.length; j++) {
+			var day2 = days2[j]
 			if (day1.id === day2.id) {
 				return true
 			}
@@ -12,25 +12,21 @@ function haveDaysInCommon(days1, days2) {
 }
 
 function addBedsCountPerWeek(retreat, participants) {
-	var beds = retreat.house.beds
-	var bedsPerWeek = []
-
 	for (var i = 0; i < retreat.weeks.length; i++) {
-		bedsPerWeek.push(beds)
+		var bedsAvailable = retreat.house.beds
 		var week = retreat.weeks[i]
 
 		// Remove a bed each time a participant is found in the week
 		for (var j = 0; j < participants.length; j++) {
 			var participant = participants[j]
 			if (haveDaysInCommon(week.days, participant.days)) {
-				bedsPerWeek[i] -= 1
+				bedsAvailable -= 1
 			}
 		}
 
 		// Add beds count to week
-		retreat.weeks[i].beds = bedsPerWeek[i]
+		retreat.weeks[i].beds = bedsAvailable
 	}
-
 	return retreat
 }
 
